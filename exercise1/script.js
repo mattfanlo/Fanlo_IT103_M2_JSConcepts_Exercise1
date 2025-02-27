1d7a54189548424379ba338dd35cd737f1123ce0
mattfanlo <mmfanlo@mcm.edu.ph>
2025-02-28 02:49:08 +0800

    Delete exercise1 directory

--- exercise1/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('changeTextBtn');
-const paragraph = document.getElementById('message');
-
-
-button.addEventListener('click', function() {
-    paragraph.textContent = 'You clicked the button!';
-    
-});
